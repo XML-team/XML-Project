0a84d1961369341728f57877c56da78a53bf2e38
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-07-01 16:31:59 +0300

    Created Students list

--- XMLThread/Program.cs
+++ XMLThread/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace XMLThread
 {
@@ -16,15 +18,31 @@ namespace XMLThread
             
         }
         
-        static async Task<List<Student>> XMLRead()
+        static async Task<List<Student>> XMLRead(Type type, string path)
         {
+            List<Student> students = null;
+
+            if (File.Exists(path))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(type);
+                TextReader reader = new StreamReader(path);
+                students = xmlSerializer.Deserialize(reader) as List<Student>;
+                reader.Close();
+            }
 
             return null;
         }
 
         static async Task<List<Student>> GetStudentList()
         {
-
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Abdullah",Age=28},
+                new Student{Id=2,Name="Sultan",Age=24},
+                new Student{Id=3,Name="Fahad",Age=27},
+                new Student{Id=4,Name="Reema",Age=25},
+                new Student{Id=5,Name="Hanan",Age=22}
+            };
             return null;
         }
         
