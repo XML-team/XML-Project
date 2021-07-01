f910baae3a0bbb2c80bd060b21c114f31fd8f71b
Reema3Alyousef <reema.alyousef@tuwaiq.edu.sa>
2021-07-01 16:20:36 +0300

    Create XML file done

--- XMLThread/Program.cs
+++ XMLThread/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace XMLThread
 {
@@ -8,20 +10,32 @@ namespace XMLThread
     {
         static void Main(string[] args)
         {
-            
+            Student Reema = new Student("Reema","Imam Abdulrahman bin Faisal University", "Computer Science");
+            SerializeToXml(typeof(Student), Reema, @"C:\Users\Reema\Desktop\StudentInfo.xml");
         }
 
-        static async Task XMLWrite(List<Student> students)
-        {
-            
-        }
-        
-        static async Task<List<Student>> XMLRead()
-        {
+        static void SerializeToXml(Type type, object o, string path)
+         {
+            if (File.Exists(path)) File.Delete(path);
+            XmlSerializer xmlSerializer = new XmlSerializer(type);
+            TextWriter writer = new StreamWriter(path);
+            xmlSerializer.Serialize(writer, o);
+            writer.Close();
+         }
 
-            return null;
-        }
+        static object DeserializeFromXml(Type type, string path)
+            {
+            object o = null;
 
+                if (File.Exists(path))
+                   {
+                    XmlSerializer xmlSerializer = new XmlSerializer(type);
+                    TextReader reader = new StreamReader(path);
+                    o = xmlSerializer.Deserialize(reader);
+                    reader.Close();
+                     }
+            return o;
+            }
         static async Task<List<Student>> GetStudentList()
         {
 
@@ -30,10 +44,23 @@ namespace XMLThread
         
     }
 
-    class Student
+    public class Student
     {
-        public int Id { get; set; }
+
+        public Student()
+        {
+                
+        }
+
+        public Student(string name, string university, string major)
+        {
+            Name = name;
+            University = university;
+            Major = major;
+        }
+
         public string Name { get; set; }
-        public int Age { get; set; }
+        public string University{ get; set; }
+        public string Major { get; set; }
     }
 }
