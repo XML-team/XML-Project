f6caa6220aec2272d4c8f50907ffefc7905d7d90
sultan <sultan.alzoubi@tuwaiq.edu.sa>
2021-07-01 15:53:25 +0300

    Add project files.

---
+++ XMLThread/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace XMLThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static async Task XMLWrite(List<Student> students)
+        {
+            
+        }
+        
+        static async Task<List<Student>> XMLRead()
+        {
+
+            return null;
+        }
+
+        static async Task<List<Student>> GetStudentList()
+        {
+
+            return null;
+        }
+        
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
